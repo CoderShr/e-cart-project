56f2985e0640f9e51825d254cc3d23c032608ec4
CoderShr <103101677+CoderShr@users.noreply.github.com>
2022-04-29 19:25:44 +0530

    Add files via upload

---
+++ ShoppingProductService/src/main/java/com/cart/ShoppingService/Model/User.java
@@ -0,0 +1,62 @@
+package com.cart.ShoppingService.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user_profiles")
+public class User {
+
+	@Id
+	private Integer id;
+	
+	private String name;
+	
+	private String email;
+
+	public User(){}
+
+	public User(int id, String email, String name) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + id + ", name=" + name + ", email=" + email + "]";
+	}
+	
+}
